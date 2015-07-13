7f251ee9a06455e539b7788d06210158db323714
unknown <remioludemi@gmail.com>
2015-07-13 19:28:04 -0400

    June is the best month

---
+++ season.py
@@ -0,0 +1,25 @@
+n = raw_input("Enter Season:")
+if n == "November":
+	print ("Winter")
+elif n == "December":
+	print ("Winter")
+elif n == "January":
+	print ("Winter")
+elif n == "February":
+	print ("Winter")
+elif n == "March":
+	print ("Winter")
+elif n == "April":
+	print ("Spring")
+elif n == "May":
+	print ("Spring")
+elif n == "June":
+	print ("Summer")
+elif n == "July":
+	print ("Summer")
+elif n == "August":
+	print ("Summer")
+elif n == "September":
+	print ("Autumn")
+elif n == "October":
+	print ("Autumn")
