ae10944da4ab1a700e88ec28f84976f07edf0cc6
unknown <remioludemi@gmail.com>
2015-07-13 18:51:10 -0400

    finished weekday problem

---
+++ week.py
@@ -0,0 +1,15 @@
+x = raw_input("Enter Day:")
+if x == "Monday":
+	print ("Weekday")
+elif x == "Tuesday": 
+	print ("Weekday")
+elif x == "Wednesday": 
+	print ("Weekday")
+elif x == "Thursday": 
+	print ("Weekday")
+elif x == "Friday": 
+	print ("Weekday")
+elif x == "Saturday":
+	print ("Weekend")
+elif x == "Sunday":
+	print ("Weekend")
